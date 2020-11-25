c461beb5af206425d0b46833b8229f0fe4a81347
Joshua Robinson <joshuarobinson@students.abtech.edu>
2020-11-24 21:41:43 -0500

    first commit

---
+++ public/bird-staff/index.php
@@ -0,0 +1,18 @@
+<?php require_once('../../private/initialize.php'); ?>
+
+<?php $page_title = 'Bird Staff Menu'; ?>
+<?php include(SHARED_PATH . '/bird-staff-header.php'); ?>
+
+<div id="content">
+  <div id="main-menu">
+    <h2>Main Menu</h2>
+    <ul>
+      <li><a href="<?php echo url_for('/bird-staff/views/index.php'); ?>">Birds</a></li>
+      <li><a href="<?php echo url_for('/bird-staff/admins/index.php'); ?>">Admin</a></li>
+
+    </ul>
+  </div>
+
+</div>
+
+<?php include(SHARED_PATH . '/bird-staff-footer.php'); ?>
